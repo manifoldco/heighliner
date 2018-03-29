e0f588382210acaccca5cf3d01e292bc8638f5e9
Jelmer Snoeck <jelmer@siphoc.com>
2018-03-29 16:50:47 +0200

    Introduce the ConfigPolicy CRD.
    This introduces the ConfigPolicy CRD. This will be used by the
    Microservice to set up correct configuration objects for the associated
    containers.
    
    The controller will monitor the values of `Env` and `EnvFrom` and store
    them sorted and hashed in the `Hashed` status value. If this value
    changes, the `LastUpdated` value will be refreshed. This is an
    indication for the Microservice controller to deploy a new version of
    the Microservice.

--- pkg/svc/svc.go
+++ pkg/svc/svc.go
@@ -103,4 +103,17 @@ var (
 			},
 		},
 	}
+
+	// ConfigPolicyResource describes the CRD configuration for the
+	// ConfigPolicy CRD.
+	ConfigPolicyResource = kubekit.CustomResource{
+		Name:       "configpolicy",
+		Plural:     "configpolicies",
+		Group:      v1alpha1.GroupName,
+		Version:    v1alpha1.Version,
+		Scope:      v1beta1.NamespaceScoped,
+		Aliases:    []string{"cp"},
+		Object:     &v1alpha1.ConfigPolicy{},
+		Validation: v1alpha1.ConfigPolicyValidationSchema,
+	}
 )
