234cf95fa3acdb2744ffcec236226be597978316
Domenic Rosati <domenicrosati@gmail.com>
2018-06-06 15:30:48 -0300

    Ignore required contexts

--- pkg/githubrepository/controller.go
+++ pkg/githubrepository/controller.go
@@ -458,6 +458,7 @@ func createGitHubDeployment(ctx context.Context, cl deploymentClient, repo *v1al
 			Description:          k8sutils.PtrString("Heighliner Deployment"),
 			Ref:                  k8sutils.PtrString(release.Tag),
 			TransientEnvironment: k8sutils.PtrBool(release.Name != repo.Spec.Repo),
+			RequiredContexts:     &[]string{},
 		}
 
 		deploy, _, err := cl.CreateDeployment(ctx, repo.Spec.Owner, repo.Spec.Repo, dpl)
